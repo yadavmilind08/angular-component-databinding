412f192b7faf1310fc5ad711f9b87c6de4741faa
Milind Yadav <yadavmilind08@gmail.com>
2021-03-24 20:04:39 +0530

    Use of Local Refrence in Template

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -16,12 +16,12 @@ export class CockpitComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onAddServer() {
-    this.serverCreated.emit({ serverName: this.newServerName, serverContent: this.newServerContent });
+  onAddServer(nameInput: HTMLInputElement) {
+    this.serverCreated.emit({ serverName: nameInput.value, serverContent: this.newServerContent });
   }
 
-  onAddBlueprint() {
-    this.blueprintCreated.emit({ serverName: this.newServerName, serverContent: this.newServerContent });
+  onAddBlueprint(nameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({ serverName: nameInput.value, serverContent: this.newServerContent });
   }
 }
 
